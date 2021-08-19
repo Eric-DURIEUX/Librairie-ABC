c480de4a4701b52897d58a687eee1b47c81be03d
Lucile Demongeot <bin011@outlook.fr>
2021-08-19 10:25:30 +0200

    Commencement JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,22 @@ console.log("test")
 fetch('assets/js/books.json')
 .then(response => response.json())
 .then((jsonBooks) => {
-
 	// récupération des UL du HTML
-	let literatureList = $('#literature');
-	let thrillerList = $('#thriller');
-	let youthList = $('#youth');
-	let historyList = $('#history');
-	let scienceList = $('#science');
+	let litteratureList = document.querySelector("#litterature");
+	let policierList = document.querySelector("#thriller");
+	let jeunesseList = document.querySelector("#youth");
+	let histoireList = document.querySelector("#history");
+	let scienceList = document.querySelector("#science");
 
+	// récupération des données
 	let histoireData = jsonBooks.histoire;
-	let littératureData = jsonBooks.littérature;
+	let litteratureData = jsonBooks.litterature;
 	let scienceData = jsonBooks.science;
 	let policierData = jsonBooks.policier;
 	let jeunesseData = jsonBooks.jeunesse;
 
-	console.log(historyData);
-	// document.getElementById('filmTable').innerHTML += bookElToInject;
-
-	let ulContent = (bookContent, bookList) =>{
-	  	bookContent.map((book) => {
-	  		console.log(jsonBooks.Histoire)
+	let ulContent = (bookData, bookList) =>{
+	  	bookData.map((book) => {
 
 		  	let title = book.original_title;
 		  	let author = book.author;
@@ -31,7 +27,7 @@ fetch('assets/js/books.json')
 		  	let poster = book.poster_path;
 		  	let price = book.price;
 		  	
-		  	bookList += `
+		  	let bookElToInject = `
 		  	<div class='col-12 col-sm-6 col-xl-4 mb-3'>
 		  	  <div class='row no-gutters'>
 		  	    <div class='col-md-5'>
@@ -40,14 +36,23 @@ fetch('assets/js/books.json')
 		  	    <div class='col-md-7 pl-2'>
 		  	      <h5 class="pt-3 pt-md-0">${title}</h5>
 		  	      <h4 class="pt-3 pt-md-0">${author}</h4>
-		  	      <p class='text'>${overview}</p>
+		  	      <p id='synopsis'>${overview}</p>
 		  	      <p>${price}</p>
 		  	    </div>
 		  	  </div>
 		  	</div>`;
+
+		bookList.innerHTML += bookElToInject;
+
   		})// map
+
+
 	} // function
 
-	// ulcontent()
+	ulContent(litteratureData, litteratureList)
+	ulContent(policierData, policierList)
+	ulContent(histoireData, histoireList)
+	ulContent(jeunesseData, jeunesseList)
+	ulContent(scienceData, scienceList)
 
 }) // then
