def346285880c4d6a1229ace9a29a4b9331b07a9
asolene <solene.allais97@gmail.com>
2021-08-19 09:30:31 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("test")
+fetch('books.json')
+.then(response => response.json())
+.then((jsonBooks) => {
+  jsonBooks.results.map((book) => {
+
+    let title = book.original_title;
+    let overview = book.overview;
+    let poster = book.poster_path;
+    let vote = book.vote_average;
+  
+    
+    let bookElToInject = `
+    <div class='col-12 col-sm-6 col-xl-4 mb-3'>
+      <div class='row no-gutters'>
+        <div class='col-md-5'>
+          <img class='img-fluid' src='${poster}' />
+        </div>
+        <div class='col-md-7 pl-2'>
+          <h5 class="pt-3 pt-md-0">${title}</h5>
+          <h4 class="pt-3 pt-md-0">${author}</h4>
+          <p class='text'>${overview}</p>
+          <p>${price}</p>
+        </div>
+      </div>
+    </div>
+  `;
+    document.getElementById('filmTable').innerHTML += bookElToInject;
+      
+  })
+})
